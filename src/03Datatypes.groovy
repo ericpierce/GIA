b04b9cf71c6483dda56cb71b040370111e259377
ericpierce <eric.d.pierce@gmail.com>
2013-10-09 14:58:14 -0500

    copy/pasta fix

--- src/03Datatypes.groovy
+++ src/03Datatypes.groovy
@@ -143,7 +143,7 @@ assert greeting.contains('Groovy')
 assert greeting[6..11] == 'Groovy'
 assert 'Hi' + greeting - 'Hello' == 'Hi Groovy!'
 assert greeting.count('o') == 3
-assert 'x'.padLeft(3) == ' x'
+assert 'x'.padLeft(3) == '  x'
 assert 'x'.padRight(3,'_') == 'x__'
 assert 'x'.center(3) == ' x '
 assert 'x' * 3 == 'xxx'
