747cdbc62ec9d38ce27fcc294989b07dea10ec7d
Eric Pierce <eric.d.pierce@gmail.com>
2013-11-12 22:37:26 -0600

    Add property file

---
+++ src/property.groovy
@@ -0,0 +1,16 @@
+// Create the properties object, and load it from the file system:
+Properties props = new Properties()
+File propsFile = new File('/home/three/temp/test.properties')
+ props.load(propsFile.newDataInputStream())
+
+// Take a peek:
+ println props.getProperty('porcupine')
+
+// Write a new random value and persist it to the file system:
+Integer rand = new Random().next(4)
+props.setProperty('porcupine', rand.toString())
+props.store(propsFile.newWriter(), null)
+
+// Peek again:
+props.load(propsFile.newDataInputStream())
+println props.getProperty('porcupine')
